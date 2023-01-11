f97670f26cfaa084fa20eae4169dddc60ce5de47
Daniel Fulbrecht <daniel@daniels-mbp-2.myfiosgateway.com>
2023-01-10 23:26:19 -0500

    US-1 Frontend tests passing

--- front-end/src/reservations/newReservations.js
+++ front-end/src/reservations/newReservations.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReservationForm from "./reservationForm";
-import ReservationsTest from "./reservationsTest";
 
 function NewReservations(){
     return (
         <div>
             <h1>New Reservations</h1>
             <ReservationForm />
-            <ReservationsTest />
         </div>
     )
 }
